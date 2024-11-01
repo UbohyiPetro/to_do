6b02cce5e81db7d0fa0a9507fd5d91b05a31a4f2
UBOHYI PETRO <90032028+UbohyiPetro@users.noreply.github.com>
2024-11-01 14:59:26 +0200

    SWTD-2: configure project structure (#2)

---
+++ lib/add_todo/ui/add_todo_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddToDoScreen extends StatelessWidget {
+  const AddToDoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/edit_todo/ui/edit_todo_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditTodoScreen extends StatelessWidget {
+  const EditTodoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
