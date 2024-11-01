3f30fe6f5e5b8c42248eab09a360083b0118d1cf
UBOHYI PETRO <90032028+UbohyiPetro@users.noreply.github.com>
2024-11-01 17:15:50 +0200

    SWTD-4: design login screen (#4)

--- lib/login/ui/login_screen.dart
+++ lib/login/ui/login_screen.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../navigation/navigation.dart';
+import '../../theme/spacing.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Align(
+            alignment: const Alignment(0, 0.5),
+            child: Padding(
+              padding: const EdgeInsets.all(Spacing.medium),
+              child: FilledButton(
+                onPressed: () {
+                  Get.toNamed(Routes.main);
+                },
+                style: FilledButton.styleFrom(
+                  elevation: 0,
+                  minimumSize: const Size(
+                    double.infinity,
+                    Spacing.defaultButton,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      Spacing.radiusExtraMedium,
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Вхід',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
