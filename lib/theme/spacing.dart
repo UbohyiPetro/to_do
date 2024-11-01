b60bd5ffdb05a64e3fae43fa44d0331124536b2d
Ubohyi Petro <upetro.2014@gmail.com>
2024-11-01 13:14:20 +0200

    SWTD-1: add spacing file

---
+++ lib/theme/spacing.dart
@@ -0,0 +1,13 @@
+class Spacing {
+  static const double tiny = 4.0;
+  static const double small = 8.0;
+  static const double medium = 16.0;
+  static const double semiLarge = 24.0;
+
+  static const defaultButton = 50.0;
+
+  static const radiusTiny = 5.0;
+  static const radiusSmall = 8.0;
+  static const radiusMedium = 10.0;
+  static const radiusExtraMedium = 15.0;
+}
